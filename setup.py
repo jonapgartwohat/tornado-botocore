9f2c8594965fbc832dba664844ceec0abb2c9c1d
ChrisPriebe <chris.priebe@2hatsecurity.com>
2018-05-04 16:52:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,11 @@ import os
 import tornado_botocore as app
 import uuid
 
-from pip.req import parse_requirements
+#@see https://stackoverflow.com/questions/25192794/no-module-named-pip-req
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 
